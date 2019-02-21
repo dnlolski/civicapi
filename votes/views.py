2e4564b1c4d611b0e8b685d908cc4400a35a9b16
dnlolski <daniel.olszewski00@gmail.com>
2019-02-21 16:02:30 +0100

    added template to display all records and some other renderers besides template

--- votes/views.py
+++ votes/views.py
@@ -1,8 +1,15 @@
 from rest_framework import generics
+from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer, BrowsableAPIRenderer
 from .models import Vote
 from .serializers import VoteSerializer 
 
 
 class VotesList(generics.ListCreateAPIView):
+    renderer_classes = (
+        JSONRenderer, 
+        TemplateHTMLRenderer, 
+        BrowsableAPIRenderer,
+        )
+    template_name = "vote_list.html"
     queryset = Vote.objects.all()
     serializer_class = VoteSerializer
